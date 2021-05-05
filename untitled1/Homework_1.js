21d4ce3adcffa311f6fa8ef4e9256770f3ed44e7
Denis <dbairamkulow@mail.ru>
2021-05-05 16:55:40 +0300

    Homework_1

---
+++ untitled1/Homework_1.js
@@ -0,0 +1,17 @@
+/*Задание №1
+/*var temp_cels = prompt("Введите температуру в Цельции");
+var temp_var = (9/5) * (+temp_cels) + 32;
+console.log((temp_var));*/
+
+/*Задание №2
+var name = "Василий";
+var admin = name;
+console.log(admin);*/
+
+/*Задание №3
+console.log(100 + "108");
+/*100108*/
+
+/*Задание №4*
+ Разобрался:))
+ */
